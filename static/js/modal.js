08cc13301976326458ef21b484263e5e353d8250
cemcannn <cmcan@windowslive.com>
2022-06-05 00:23:16 +0300

    Main | modal.js is fixed

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,12 +1,13 @@
 /** modal-image js **/
 
 // Get the modal
-var modal = document.getElementById("myModal");
+var modal = document.getElementById('myModal');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
-var img = document.getElementById("myImg");
+var imgs = document.getElementsByClassName("card-img-top");
 var modalImg = document.getElementById("img01");
 var captionText = document.getElementById("caption");
+Array.prototype.forEach.call(imgs, function(img) {
 img.onclick = function(){
   modal.style.display = "block";
   modalImg.src = this.src;
@@ -19,4 +20,18 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
-}
+}});
+
+$(function() {
+  var selectedClass = "";
+  $(".fil-cat").click(function() {
+    selectedClass = $(this).attr("data-rel");
+    $("#portfolio").fadeTo(100, 0.1);
+    $("#portfolio div").not("." + selectedClass).fadeOut().removeClass('scale-anm');
+    setTimeout(function() {
+      $("." + selectedClass).fadeIn().addClass('scale-anm');
+      $("#portfolio").fadeTo(300, 1);
+    }, 300);
+
+  });
+});
